e3afbe99ceb34cf9005aa7786792e4471ce11020
Danny Tsuei <blueshaedow@gmail.com>
2016-05-27 21:47:18 -0700

    Removing .dep folder

--- firmware/keyboard/polarbear/.dep/obj_polarbear_tester_pjr_common_avr_bootloader.o.d
+++
@@ -1,7 +0,0 @@
-obj_polarbear_tester_pjr/common/avr/bootloader.o:  \
- ../../tmk_core/common/avr/bootloader.c config.h \
-  ../../tmk_core/common/bootloader.h
-
-config.h:
-
-../../tmk_core/common/bootloader.h:
--- firmware/keyboard/polarbear/.dep/obj_polarbear_tester_pjrc_common_sleep_led.o.d
+++
@@ -1,9 +0,0 @@
-obj_polarbear_tester_pjrc/common/sleep_led.o:  \
- ../../tmk_core/common/sleep_led.c config.h ../../tmk_core/common/led.h \
-  ../../tmk_core/common/sleep_led.h
-
-config.h:
-
-../../tmk_core/common/led.h:
-
-../../tmk_core/common/sleep_led.h:
